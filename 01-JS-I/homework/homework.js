11f8c65fd6a89abafdad917bfdfd8c6f2f67550b
n_narcise <joaquin.narcisse@gmail.com>
2022-09-13 19:24:25 -0500

    nothing

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -11,7 +11,7 @@ const nuevoNum = 0;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5  === 5;
+const nuevaResta = 10 -5  === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
